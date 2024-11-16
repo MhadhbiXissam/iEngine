4d1c3459b89c7df15bfcbc17933cc9006f59a612
Mhadhbi Issam <mhadhbiXissam@gmail.com>
2024-11-16 01:06:06 +0100

    Update sdl.sh

--- scripts/linux/sdl.sh
+++ scripts/linux/sdl.sh
@@ -1,7 +1,18 @@
+set -e -x 
+mkdir -p .tmp || true 
+pushd .tmp 
+mkdir -p linux || true 
+cd linux 
+rm -rf .tmp/linux/SDL || true 
 git clone https://github.com/libsdl-org/SDL
 cd SDL
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release --parallel
-sudo cmake --install . --config Release
+#sudo cmake --install . --config Release
+popd 
+cp -rf .tmp/linux/SDL/include/* include
+cp .tmp/linux/SDL/build/libSDL3.so lib/linux
+cp -rf .tmp/linux/SDL/docs docs/SDL
+rm -rf .tmp/linux/SDL || true 
